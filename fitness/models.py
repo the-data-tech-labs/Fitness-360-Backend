864f15197e7b014340adeff0dd02ddb547bafb2b
yashrajnimbalkar <yashnimbalkar1111@gmail.com>
2025-07-30 09:30:50 +0530

    Update models.py

--- fitness/models.py
+++ fitness/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.contrib.auth.models import User  # Import User model
-
 class FitnessInput(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)  # Link input to a user
     
